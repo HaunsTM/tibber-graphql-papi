bf59f3b89992f89b17ccbafa1214520178767e7a
HaunsTM <hans.baldetorp@gmail.com>
2020-08-26 07:56:34 +0200

    start building a client

---
+++ clientTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clientTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
