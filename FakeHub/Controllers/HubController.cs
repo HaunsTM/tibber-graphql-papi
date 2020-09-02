079c9a716cc1ac58482c28ecee2d64cca0bd38c7
HaunsTM <hans.baldetorp@gmail.com>
2020-09-02 12:18:54 +0200

    Added a fake client hub for demo purposes

---
+++ FakeHub/Controllers/HubController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FakeHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HubController : ControllerBase
+    {
+
+        [HttpGet]
+        public string TakeAction(string device, string actionToPerform)
+        {
+            var message = $"An action call {actionToPerform} were sent to {device}.";
+            return message;
+        }
+    }
+}
